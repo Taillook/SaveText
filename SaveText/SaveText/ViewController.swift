5e057a40487e4cf01c7f3e1fcde4d0cab8b07bc8
Taillook <jousoku@gmail.com>
2017-11-09 11:59:25 +0900

    [Update] text edit

--- SaveText/SaveText/ViewController.swift
+++ SaveText/SaveText/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.becomeFirstResponder()
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillShow(notification:)),
+            name: NSNotification.Name.UIKeyboardWillShow,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: NSNotification.Name.UIKeyboardWillHide,
+            object: nil
+        )
     }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardRectValue = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.textView.frame = CGRect(x:0,y:0,width:view.frame.width,height:view.frame.height - keyboardRectValue.height)
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.textView.frame = CGRect(x:0,y:0,width:view.frame.width,height:view.frame.height)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
